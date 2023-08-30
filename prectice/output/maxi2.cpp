197af752b8112059e181af26db839417afc2a82b
bornabhowmik <bornabhowmik4@gmail.com>
2023-08-30 21:42:16 +0600

    update code added

--- prectice/output/maxi2.cpp
+++ prectice/output/maxi2.cpp
@@ -1,17 +1,14 @@
 #include<iostream>
-
 using namespace std;
-
-int my_min(int a, int b){
-    if(a<b) return a;
+int my_maxi(int a, int b){
+    if(a>b) return a;
     else return b;
 }
-
 int main(){
     int a,b;
     cin>>a>>b;
-    int mn=my_min(a,b);
-    cout<<mn<<endl;
+    int mx=my_maxi(a,b);
+    cout<<mx<<endl;
 
     return 0;
 }
